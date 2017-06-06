8dcbfd7fe92b87a877379c6169a0690a1afc4db0
gyunaymuradov <gmuradov93@gmail.com>
2017-06-06 22:43:01 +0300

    Added new exercises

--- MethodsAndDebugging-Exercise/GeometryCalculator/Program.cs
+++ MethodsAndDebugging-Exercise/GeometryCalculator/Program.cs
@@ -10,7 +10,61 @@ namespace GeometryCalculator
     {
         static void Main(string[] args)
         {
+            var figureType = Console.ReadLine();
 
+            switch (figureType)
+            {
+                case "triangle":
+                    var triangleSide = double.Parse(Console.ReadLine());
+                    var height = double.Parse(Console.ReadLine());
+
+                    var triangleArea = GetTriangleArea(triangleSide, height);
+
+                    Console.WriteLine($"{triangleArea:F2}");
+                    break;
+                case "square":
+                    var squareSide = double.Parse(Console.ReadLine());
+
+                    var squareaArea = GetSquareArea(squareSide);
+
+                    Console.WriteLine($"{squareaArea:F2}");
+                    break;
+                case "rectangle":
+                    var width = double.Parse(Console.ReadLine());
+                    var rectangleHeight = double.Parse(Console.ReadLine());
+
+                    var rectangleArea = GetRectangleArea(width, rectangleHeight);
+
+                    Console.WriteLine($"{rectangleArea:F2}");
+                    break;
+                case "circle":
+                    var radius = double.Parse(Console.ReadLine());
+
+                    var circleArea = GetCircleArea(radius);
+
+                    Console.WriteLine($"{circleArea:F2}");
+                    break;
+            }
+        }
+
+        static double GetCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        static double GetRectangleArea(double width, double rectangleHeight)
+        {
+            return width * rectangleHeight;
+        }
+
+        static double GetSquareArea(double squareSide)
+        {
+            return squareSide * squareSide;
+        }
+
+        static double GetTriangleArea(double triangleSide, double height)
+        {
+            return triangleSide * height / 2;
         }
     }
 }
---
+++ MethodsAndDebugging-Exercise/MasterNumber/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = long.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (IsPalindrome(i) && SumOfDigitsDivisableBy7(i) && ContainsEvenDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool ContainsEvenDigit(long number)
+        {
+            bool containsEvenDigit = false;
+            while (number > 10)
+            {
+                long digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    containsEvenDigit = true;
+                    break;
+                }
+                number /= 10;
+            }
+            return containsEvenDigit;
+        }
+
+        static bool SumOfDigitsDivisableBy7(long number)
+        {
+            bool divisableBy7 = false;
+            long sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            if (sum % 7 == 0)
+            {
+                divisableBy7 = true;
+            }
+            return divisableBy7;
+        }
+
+        static bool IsPalindrome(long number)
+        {
+            bool isPalindrome = false;
+            var temp = number;
+            long newNumber = 0;
+            while (number > 0)
+            {
+                long digit = number % 10;
+                newNumber = (newNumber * 10) + digit;
+                number /= 10;
+            }
+            if (newNumber == temp)
+            {
+                isPalindrome = true;
+            }
+            return isPalindrome;
+        }
+    }
+}
