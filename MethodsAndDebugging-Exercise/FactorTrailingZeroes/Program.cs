ac6a6e248fdba300f6375b949a7fb408914bbe52
gyunaymuradov <gmuradov93@gmail.com>
2017-06-07 11:53:59 +0300

    Added new exercises

---
+++ MethodsAndDebugging-Exercise/FactorTrailingZeroes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorTrailingZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var factorial = GetFactorial(n);
+            var zeroesCount = GetZeroesCount(factorial);
+
+            Console.WriteLine(zeroesCount);
+
+        }
+
+        static long GetZeroesCount(BigInteger factorial)
+        {
+            long zeroesCounter = 0;
+            while (factorial % 10 == 0)
+            {
+                factorial /= 10;
+                zeroesCounter++;
+            }
+            return zeroesCounter;
+        }
+
+        static BigInteger GetFactorial(long n)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ MethodsAndDebugging-Exercise/Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            
+            Console.WriteLine(GetFactorial(n));
+        }
+
+        static BigInteger GetFactorial(long n)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
