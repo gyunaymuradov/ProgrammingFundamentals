fd9da350e6d6cfb2ec168ab592d49a411cf59ea9
gyunaymuradov <gmuradov93@gmail.com>
2017-06-08 21:18:40 +0300

    Added new exercises

---
+++ Arrays-Lab/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            while (arr.Length > 1)
+            {
+                int[] condensed = new int[arr.Length - 1];
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = arr[i] + arr[i + 1];
+                }
+                arr = condensed;
+            }
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
