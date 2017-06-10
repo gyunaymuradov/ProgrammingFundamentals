18f7cce41b188812d0cb6bb7b12ed60c93dba0c4
gyunaymuradov <gmuradov93@gmail.com>
2017-06-10 22:20:45 +0300

    Added new exercises

---
+++ Arrays-Exercises/LargestCommonEnd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestCommonEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOne = Console.ReadLine().Split(' ').ToArray();
+            string[] arrayTwo = Console.ReadLine().Split(' ').ToArray();
+
+            int counterForTheLeftSide = 0;
+            int counterForTheRightSide = 0;
+
+            var smallerArraylength = Math.Min(arrayOne.Length, arrayTwo.Length);
+            for (int i = 0; i < smallerArraylength; i++)
+            {
+                if (arrayOne[i] == arrayTwo[i])
+                {
+                    counterForTheLeftSide++;
+                }
+                if (arrayOne[arrayOne.Length - i - 1] == arrayTwo[arrayTwo.Length - i - 1])
+                {
+                    counterForTheRightSide++;
+                }
+            }
+            Console.WriteLine(Math.Max(counterForTheRightSide, counterForTheLeftSide));
+        }
+    }
+}
---
+++ Arrays-Exercises/RotateAndSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var rotationsCount = int.Parse(Console.ReadLine());
+
+            var sumArray = new int[array.Length];
+            for (int i = 0; i < rotationsCount; i++)
+            {
+                RotateArray(array);
+
+                for (int j = 0; j < array.Length; j++)
+                {
+                    sumArray[j] += array[j];
+                }
+            }
+            foreach (var currentParameter in sumArray)
+            {
+                Console.Write($"{currentParameter} ");
+            }
+
+        }
+
+        static void RotateArray(int[] array)
+        {
+            var lastElement = array[array.Length - 1];
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[0] = lastElement;
+        }
+    }
+}
