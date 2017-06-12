49dde1a0b43ae828c955850762c4422a3e884fd0
gyunaymuradov <gmuradov93@gmail.com>
2017-06-12 22:31:42 +0300

    Added new exercises

---
+++ ArraysAndMethods-MoreExercises/GrabAndGo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrabAndGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] arr = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            int numToSearch = int.Parse(Console.ReadLine());
+
+            long sum = 0;
+            bool occurrenceFound = false;
+            for (long i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == numToSearch)
+                {
+                    sum = 0;
+                    for (long j = i - 1; j >= 0; j--)
+                    {
+                        sum += arr[j];
+                        occurrenceFound = true;
+                    }
+                }
+            }
+            if (occurrenceFound)
+            {
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+        }
+    }
+}
---
+++ ArraysAndMethods-MoreExercises/PizzaIngredients/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ingredients = Console.ReadLine().Split().ToArray();
+            var lengthOfIngredient = int.Parse(Console.ReadLine());
+            var maxIngredients = 10;
+            var counter = 0;
+
+            List<string> list = new List<string>();
+
+            foreach (var currentIngredient in ingredients)
+            {
+                if (currentIngredient.Length == lengthOfIngredient)
+                {
+                    Console.WriteLine($"Adding {currentIngredient}.");
+                    maxIngredients--;
+                    counter++;
+                    list.Add(currentIngredient);
+                }
+                if (maxIngredients == 0)
+                {
+                    Console.WriteLine($"Made pizza with total of {counter} ingredients.");
+                    Console.WriteLine($"The ingredients are: {string.Join(", ", list)}.");
+                    return;
+                }
+            }
+            Console.WriteLine($"Made pizza with total of {counter} ingredients.");
+            Console.WriteLine($"The ingredients are: {string.Join(", ", list)}.");
+        }
+    }
+}
