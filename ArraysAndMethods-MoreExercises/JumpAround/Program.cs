6e64c69b178224b78dfff52af8428339b449c8c6
gyunaymuradov <gmuradov93@gmail.com>
2017-06-13 15:40:36 +0300

    Added new exercises

---
+++ ArraysAndMethods-MoreExercises/JumpAround/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int index = 0;
+            var sum = 0;
+
+            while (true)
+            {
+                int tempIndex = index;
+                sum += arr[index];
+                index += arr[index];
+
+                if (index >= arr.Length)
+                {
+                    index = tempIndex - arr[tempIndex];
+                    if (index < 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
