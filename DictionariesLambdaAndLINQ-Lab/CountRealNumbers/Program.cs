93f2d22344f06a8fd5e08cb80ba636cb3780da2f
gyunaymuradov <gmuradov93@gmail.com>
2017-06-15 18:02:48 +0300

    Added more exercises

---
+++ DictionariesLambdaAndLINQ-Lab/CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            var dict = new SortedDictionary<double, int>();
+
+            foreach (var num in list)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num]++;
+                }
+                else
+                {
+                    dict[num] = 1;
+                }
+            }
+            foreach (var num in dict.Keys)
+            {
+                Console.WriteLine($"{num} -> {dict[num]} times");
+            }
+        }
+    }
+}
