edfe59e34478e98d192a82ff6f74546f9a8c9e08
gyunaymuradov <gmuradov93@gmail.com>
2017-06-26 15:55:09 +0300

    Added some more exercises

---
+++ ExamPreparation-I/SinoTheWalker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinoTheWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leavingTime = DateTime.ParseExact(Console.ReadLine(), "HH:mm:ss", CultureInfo.InvariantCulture);
+            long stepsNeeded = long.Parse(Console.ReadLine()) % 86400;
+            long timeInSecondsForEachStep = long.Parse(Console.ReadLine()) % 86400;
+
+            long totalNeededTimeInSeconds = stepsNeeded * timeInSecondsForEachStep;
+            TimeSpan ts = TimeSpan.FromSeconds(totalNeededTimeInSeconds);
+            var arrivalTime = leavingTime + ts;
+
+            Console.WriteLine($"Time Arrival: {arrivalTime:HH:mm:ss}");
+
+        }
+    }
+}
